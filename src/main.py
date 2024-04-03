c1a61830da466bc1ee9733f9ad39f36187aca125
Giorgio Gamba <giorgiogamba97@gmail.com>
2024-04-03 15:01:24 +0200

    fix command creation

--- src/main.py
+++ src/main.py
@@ -4,8 +4,8 @@ import os
 
 # Sends a nofification to the user through the OS with passed information in it
 def sendNotification(message, title):
-    command = 'display notification "{0}" {1}'.format(title, message)
-    os.system("osascript -e {0}".format(command))
+    command = 'display notification "{0}" with title "{1}"'.format(title, message)
+    os.system("osascript -e '{0}' ".format(command))
 
 if __name__ == '__main__':
     sendNotification("TEST TITLE", "TEST MSG")
