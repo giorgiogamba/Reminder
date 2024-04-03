55b0fae00d1c101951aced2b255b5b0871e268e4
Giorgio Gamba <giorgiogamba97@gmail.com>
2024-04-03 15:07:57 +0200

    add complete message creation

--- src/main.py
+++ src/main.py
@@ -3,9 +3,21 @@
 import os
 
 # Sends a nofification to the user through the OS with passed information in it
-def sendNotification(message, title):
-    command = 'display notification "{0}" with title "{1}"'.format(title, message)
+def sendNotification(message, title, subtitle, soundName):
+    command = 'display notification "{0}"'.format(title)
+
+    if (title is not None):
+        command += ' with title "{0}"'.format(title)
+
+    if (subtitle is not None):
+        command += ' subtitle "{0}"'.format(subtitle)
+
+    if (soundName is not None):
+        command += ' sound name "{0}"'.format(soundName)
+
+    print(command)
+
     os.system("osascript -e '{0}' ".format(command))
 
 if __name__ == '__main__':
-    sendNotification("TEST TITLE", "TEST MSG")
+    sendNotification("TEST TITLE", "TEST MSG", "TEST SUBTITLE", "Pop")
